638eced5b56046a34af731290fd28557f57a3490
NGrant <ngrant@tssg.org>
2018-10-15 12:54:33 +0300

    comments to dockerrun.sh

--- docker/dockerrun.sh
+++ docker/dockerrun.sh
@@ -1,11 +1,12 @@
 # !/bin/bash
 
+# container name variable
 CONTAINER="doctave_runner"
-
+# remove any container with same name if it exists
 if [ "$(docker ps -q -a -f name=${CONTAINER})" ]; then
   docker rm -f -v ${CONTAINER}
 fi
-
+# command to boot container
 docker run \
   --name ${CONTAINER} \
   -u doctave \
