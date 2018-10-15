3096bba4990dc3946cd670138318bd99b097ea6d
NGrant <ngrant@tssg.org>
2018-10-15 12:53:12 +0300

    tabs

--- docker/dockerrun.sh
+++ docker/dockerrun.sh
@@ -3,11 +3,11 @@
 CONTAINER="doctave_runner"
 
 if [ "$(docker ps -q -a -f name=${CONTAINER})" ]; then
-    docker rm -f -v ${CONTAINER}
+  docker rm -f -v ${CONTAINER}
 fi
 
 docker run \
-    --name ${CONTAINER} \
-    -u doctave \
-    -i -t doctave \
-    /bin/bash -c "octave --no-gui"
+  --name ${CONTAINER} \
+  -u doctave \
+  -i -t doctave \
+  /bin/bash -c "octave --no-gui"
